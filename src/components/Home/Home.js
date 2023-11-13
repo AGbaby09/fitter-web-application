5e6e4cf38d2a171508031d0cd1d631fe3d5e5501
AGbaby09 <agbotsem@gmail.com>
2023-11-13 15:14:45 +0000

    homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,20 @@
 import "./Home.css";
+import Car from "../../assets/car.jpg"
 const Home = () => {
     return ( 
         <main>
-            
+            <div className="texts">
+                <h1>Fitter</h1>
+
+                <h3>WELCOME</h3>
+                <p>Let's Get Started</p>
+            </div>
+            <div className="carImage-container">
+                <img src={Car} alt="vehicle" />
+            </div>
+            <div className="log-links">
+
+            </div>
         </main>
     );
 }
