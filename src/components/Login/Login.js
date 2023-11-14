2061b32294f698594965fb7a3d40dcfdc0f636a9
AGbaby09 <agbotsem@gmail.com>
2023-11-14 22:50:02 +0000

    login and registration page done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,43 @@
 import "./Login.css";
+import { Link } from "react-router-dom";
+
 const Login = () => {
     return ( 
         <section id="login">
-            
+            <form action="" method="post">
+
+                <div className="form-title">
+                    <h1>Log In</h1>
+                </div>
+
+                <div className="radios">
+                    <label>
+                        <input type="radio" name="user" id="user" value="user"/>
+                        User
+                    </label>
+                    <label>
+                        <input type="radio" name="user" id="mechanic" value="user"/>
+                        Mechanic
+                    </label>
+                </div>
+
+                <div className="inputs">
+                    <input type="text" name="username" id="username" placeholder="Username (abcdefg)" autoComplete="false"/>
+                    <input type="password" name="password" id="password" placeholder="Password"/>
+                </div>
+
+                <div className="forgot">
+                    <Link to="/forgot">Forgot Password?</Link>
+                </div>
+
+                <button>LOG IN</button>
+
+                <div id="toRegister">
+                    <p>Don't have an account?</p>
+                    <Link to="/register">Register</Link>
+                </div>
+
+            </form>
         </section>
     );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,29 @@
 import "./Register.css";
+import { Link } from "react-router-dom";
+
 const Register = () => {
     return ( 
-        <section>
-            
+        <section id="register">
+            <form action="" method="post">
+
+                <div className="form-title">
+                    <h1>Registration</h1>
+                </div>
+
+                <div className="inputs">
+                    <input type="text" name="username" id="username" placeholder="Username (abcdefg)" autoComplete="false"/>
+                    <input type="email" name="email" id="email" placeholder="Email" autoComplete="false"/>
+                    <input type="password" name="password" id="password" placeholder="Password"/>
+                </div>
+
+                <button>Register</button>
+
+                <div id="toLogin">
+                    <p>Already have an account?</p>
+                    <Link to="/login">Login here</Link>
+                </div>
+
+            </form>
         </section>
     );
 }
