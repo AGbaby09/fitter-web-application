011abd86d0a632ee086b594f08573a4b419b83b9
AGbaby09 <agbotsem@gmail.com>
2023-11-15 19:49:03 +0000

    nav-bar progress

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
 import "./Navbar.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const Navbar = () => {
     const [Open, setOpen] = useState(false);
     const openMenu = () => setOpen(true);
     const closeMenu = () => setOpen(false);
+
+    const navigate = useNavigate();
+    const profile = () => {
+        navigate("/user/profile");
+        setOpen(false);
+    }
+    const profileUpdate = () => {
+        navigate("/user/profile/update");
+        setOpen(false);
+    }
+    const location = () => {
+        navigate("/user/location");
+        setOpen(false);
+    }
 
     return ( 
         <header id="navbar">
@@ -17,6 +32,17 @@ const Navbar = () => {
                     <button id="close" onClick={closeMenu}>
                         <i className='bx bx-x' ></i>
                     </button>
+                </div>
+                <div className="links">
+                    <div className="link">
+                        <button onClick={profile}><i className='bx bxs-user' ></i>Profile</button>
+                    </div>
+                    <div className="link">
+                        <button onClick={profileUpdate}><i className='bx bxs-car-mechanic'></i>Request Assistance</button>
+                    </div>
+                    <div className="link">
+                        <button onClick={location}><i className='large material-icons' >location_on</i>Find Nearby Shop</button>
+                    </div>
                 </div>
             </div>
         </header>
