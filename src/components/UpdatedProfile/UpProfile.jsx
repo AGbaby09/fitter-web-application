78779afb3288531205edb2a3f117e80fc6260def
AGbaby09 <agbotsem@gmail.com>
2023-11-16 18:41:04 +0000

    update profile page done

--- src/components/UpdatedProfile/UpProfile.jsx
+++ src/components/UpdatedProfile/UpProfile.jsx
@@ -3,6 +3,14 @@ const UpProfile = () => {
     return ( 
         <section id="profileUpdate">
             <h1>Update Profile</h1>
+
+            <form action="" method="post">
+            <label>Name<input type="text" placeholder="Full Name"/></label>
+                <label>Email<input type="email" placeholder="example@exam.com"/></label>
+                <label>Mobile<input type="number" placeholder="+1 (234) 456 7890"/></label>
+            </form>
+
+            <button>Update Profile</button>
         </section>
     );
 }
